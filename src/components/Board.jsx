afbc0b410c5c4bc3205ef14371cefb9716100fe9
Devika T S <tsdevika2000@gmail.com>
2022-11-17 20:33:29 +0530

    added sass,get to know styles

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,21 +3,21 @@ import Square from './Square';
 
 const board = () => {
   return (
-    <div>
-      <div>
-        <Square value={0}/>
-        <Square value={1}/>
-        <Square value={2}/>
+    <div className="board">
+      <div className="board-row">
+        <Square value={0} />
+        <Square value={1} />
+        <Square value={2} />
       </div>
-      <div>
+      <div className="board-row">
         <Square value={3} />
-        <Square value={4}/>
+        <Square value={4} />
         <Square value={5} />
       </div>
-      <div>
+      <div className="board-row">
         <Square value={6} />
-        <Square value={7}/>
-        <Square value={8}/>
+        <Square value={7} />
+        <Square value={8} />
       </div>
     </div>
   );
